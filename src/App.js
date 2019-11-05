daca7508a3cfd2b461fbd8ce03382fa0a8751f5d
Fadzilah Ariffin <fadzilah9595@gmail.com>
2019-11-05 11:56:02 +0800

    useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { 
   View,
   Text,
@@ -8,41 +8,26 @@ import {
 } from "react-native";
 
 
-class App extends Component {
-  constructor(){
-    super()
-    this.state ={
-      name: 'john',
-      class:'mobile app development',
-    }
-  }
+ function App() {
 
-  changeName(){
-    if(this.state.name == 'john')
-    { 
-      this.setState({
-        name:'johny'
-      })
-    }
-    else{
-      this.setState({
-        name:'john'
-      })
-    }
-  }
+    const [name, setName] = useState(['fadzilah']);
 
-  render() {
+    function changeName(){
+      if(name == 'fadzilah')
+      setName('ahmad')
+      else
+      setName('fadzilah')
+    }
+
     return (
       <SafeAreaView style={styles.container}>
-          <Text>{this.state.name}</Text>
-          <Text>{this.state.class}</Text>
-          <TouchableOpacity style={{backgroundColor:'blue'}} onPress={()=>this.changeName()}>
-            <Text style={{color:'white'}}>Change Name</Text>
-          </TouchableOpacity>
+        <Text> {name} </Text>
+        <TouchableOpacity style={{backgroundColor:'blue'}} onPress={()=>changeName()}>
+          <Text style={{color:'white'}}>Change Name</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     );
   }
-}
 export default App;
 
 const styles = StyleSheet.create({
